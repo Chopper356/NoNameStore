a9c9bc148e9bbcb32418034eb1bee42275a75615
Chopper356 <vedmedenko.nikita345@gmail.com>
2023-03-02 17:22:14 +0200

    added features section

--- src/pages/Home/components/Banners/index.jsx
+++ src/pages/Home/components/Banners/index.jsx
@@ -6,78 +6,80 @@ import styles from "./banners.module.scss";
 function Banners() {
 	return (
 		<div className={classNames(styles.slider_banner)}>
-			<Carousel cols={1} rows={1} gap={10} loop showDots="true" hideArrow="true" autoplay={5000} containerStyle={{height: "fit-content"}}>
-				<Carousel.Item>
-					<div className={classNames(styles.slider_item)}>
-						<img src="https://cdn.shopify.com/s/files/1/0620/5082/8457/files/main-banner-1_830x550.jpg?v=1655701492;" alt="banner_image" />
-					
-						<div className={classNames(styles.info)}>
-							<div className={classNames(styles.title)}>SUPERCHARGED FOR PROS.</div>
-							<div className={classNames(styles.subtitle)}>iPad S13+ Pro.</div>
-							<div className={classNames(styles.desc)}>From $999.00 or $41.62/mo. <br /> for 24 mo. Footnote*</div>
-							<button>Buy now</button>
+			<div className={classNames(styles.content)}>
+				<Carousel cols={1} rows={1} gap={10} loop showDots="true" hideArrow="true" autoplay={5000} containerStyle={{height: "fit-content"}}>
+					<Carousel.Item>
+						<div className={classNames(styles.slider_item)}>
+							<img src="https://cdn.shopify.com/s/files/1/0620/5082/8457/files/main-banner-1_830x550.jpg?v=1655701492;" alt="banner_image" />
+						
+							<div className={classNames(styles.info)}>
+								<div className={classNames(styles.title)}>SUPERCHARGED FOR PROS.</div>
+								<div className={classNames(styles.subtitle)}>iPad S13+ Pro.</div>
+								<div className={classNames(styles.desc)}>From $999.00 or $41.62/mo. <br /> for 24 mo. Footnote*</div>
+								<button>Buy now</button>
+							</div>
 						</div>
-					</div>
-				</Carousel.Item>
+					</Carousel.Item>
 
-				<Carousel.Item>
-					<div className={classNames(styles.slider_item)}>
-						<img src="https://cdn.shopify.com/s/files/1/0620/5082/8457/files/main-banner-2_830x550.jpg?v=1655455867;" alt="banner_image" />
-					
-						<div className={classNames(styles.info)}>
-							<div className={classNames(styles.title)}>Best sale</div>
-							<div className={classNames(styles.subtitle)}>Laptops max</div>
-							<div className={classNames(styles.desc)}>From $1699.00 or <br /> $64.62/mo.</div>
-							<button>Buy now</button>
+					<Carousel.Item>
+						<div className={classNames(styles.slider_item)}>
+							<img src="https://cdn.shopify.com/s/files/1/0620/5082/8457/files/main-banner-2_830x550.jpg?v=1655455867;" alt="banner_image" />
+						
+							<div className={classNames(styles.info)}>
+								<div className={classNames(styles.title)}>Best sale</div>
+								<div className={classNames(styles.subtitle)}>Laptops max</div>
+								<div className={classNames(styles.desc)}>From $1699.00 or <br /> $64.62/mo.</div>
+								<button>Buy now</button>
+							</div>
 						</div>
-					</div>
-				</Carousel.Item>
-			</Carousel>
+					</Carousel.Item>
+				</Carousel>
 
-			<div className={classNames(styles.items)}>
-				<span className={classNames(styles.item)}>
-					<div className={classNames(styles.wrapper)}>
-						<img src="https://cdn.shopify.com/s/files/1/0620/5082/8457/files/catbanner-01.jpg?v=1655701506" alt="banner_image" />
-						<div className={classNames(styles.info)}>
-							<div className={classNames(styles.title)}>Best sale</div>
-							<div className={classNames(styles.subtitle)}>Laptops max</div>
-							<div className={classNames(styles.desc)}>From $1699.00 or <br /> $64.62/mo.</div>
+				<div className={classNames(styles.items)}>
+					<span className={classNames(styles.item)}>
+						<div className={classNames(styles.wrapper)}>
+							<img src="https://cdn.shopify.com/s/files/1/0620/5082/8457/files/catbanner-01.jpg?v=1655701506" alt="banner_image" />
+							<div className={classNames(styles.info)}>
+								<div className={classNames(styles.title)}>Best sale</div>
+								<div className={classNames(styles.subtitle)}>Laptops max</div>
+								<div className={classNames(styles.desc)}>From $1699.00 or <br /> $64.62/mo.</div>
+							</div>
 						</div>
-					</div>
-				</span>
+					</span>
 
-				<span className={classNames(styles.item)}>
-					<div className={classNames(styles.wrapper)}>
-						<img src="https://cdn.shopify.com/s/files/1/0620/5082/8457/files/catbanner-03.jpg?v=1654859212" alt="banner_image" />
-						<div className={classNames(styles.info)}>
-							<div className={classNames(styles.title)}>New arrival</div>
-							<div className={classNames(styles.subtitle)}>Buy iPad air</div>
-							<div className={classNames(styles.desc)}>From $599 or <br /> $49.91/mo. for 12 mo.</div>
+					<span className={classNames(styles.item)}>
+						<div className={classNames(styles.wrapper)}>
+							<img src="https://cdn.shopify.com/s/files/1/0620/5082/8457/files/catbanner-03.jpg?v=1654859212" alt="banner_image" />
+							<div className={classNames(styles.info)}>
+								<div className={classNames(styles.title)}>New arrival</div>
+								<div className={classNames(styles.subtitle)}>Buy iPad air</div>
+								<div className={classNames(styles.desc)}>From $599 or <br /> $49.91/mo. for 12 mo.</div>
+							</div>
 						</div>
-					</div>
-				</span>
+					</span>
 
-				<span className={classNames(styles.item)}>
-					<div className={classNames(styles.wrapper)}>
-						<img src="https://cdn.shopify.com/s/files/1/0620/5082/8457/files/catbanner-02.jpg?v=1654859195" alt="banner_image" />
-						<div className={classNames(styles.info)}>
-							<div className={classNames(styles.title)}>15% off</div>
-							<div className={classNames(styles.subtitle)}>Smartwatch 7</div>
-							<div className={classNames(styles.desc)}>Shop the latest <br /> band styles and colors.</div>
+					<span className={classNames(styles.item)}>
+						<div className={classNames(styles.wrapper)}>
+							<img src="https://cdn.shopify.com/s/files/1/0620/5082/8457/files/catbanner-02.jpg?v=1654859195" alt="banner_image" />
+							<div className={classNames(styles.info)}>
+								<div className={classNames(styles.title)}>15% off</div>
+								<div className={classNames(styles.subtitle)}>Smartwatch 7</div>
+								<div className={classNames(styles.desc)}>Shop the latest <br /> band styles and colors.</div>
+							</div>
 						</div>
-					</div>
-				</span>
+					</span>
 
-				<span className={classNames(styles.item)}>
-					<div className={classNames(styles.wrapper)}>
-						<img src="https://cdn.shopify.com/s/files/1/0620/5082/8457/files/catbanner-04.jpg?v=1654859228" alt="banner_image" />
-						<div className={classNames(styles.info)}>
-							<div className={classNames(styles.title)}>Free engraving</div>
-							<div className={classNames(styles.subtitle)}>AirPods max</div>
-							<div className={classNames(styles.desc)}>High-fidelity playback & <br /> ultra-low distortion</div>
+					<span className={classNames(styles.item)}>
+						<div className={classNames(styles.wrapper)}>
+							<img src="https://cdn.shopify.com/s/files/1/0620/5082/8457/files/catbanner-04.jpg?v=1654859228" alt="banner_image" />
+							<div className={classNames(styles.info)}>
+								<div className={classNames(styles.title)}>Free engraving</div>
+								<div className={classNames(styles.subtitle)}>AirPods max</div>
+								<div className={classNames(styles.desc)}>High-fidelity playback & <br /> ultra-low distortion</div>
+							</div>
 						</div>
-					</div>
-				</span>
+					</span>
+				</div>
 			</div>
 		</div>
 	);	
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,5 @@
 import classNames from "classnames";
+import { Carousel } from '@trendyol-js/react-carousel';
 import Banners from "./components/Banners";
 
 import styles from "./home.module.scss";
@@ -7,6 +8,58 @@ function Home() {
 	return (
 		<div className={classNames(styles.home)}>
 			<Banners />
+
+			<div className={classNames(styles.content)}>
+
+				<div className={classNames(styles.features)}>
+					<Carousel show={5} slide={1} swiping={true} hideArrows="true">
+						<div className={classNames(styles.feature)}>
+							<i class="far fa-truck-container"></i>
+
+							<div className={classNames(styles.info)}>
+								<div className={classNames(styles.title)}>Free Shipping</div>
+								<div className={classNames(styles.subtittle)}>From all orders over $100</div>
+							</div>
+						</div>
+
+						<div className={classNames(styles.feature)}>
+							<i class="far fa-gift"></i>
+
+							<div className={classNames(styles.info)}>
+								<div className={classNames(styles.title)}>Free Shipping</div>
+								<div className={classNames(styles.subtittle)}>From all orders over $100</div>
+							</div>
+						</div>
+
+						<div className={classNames(styles.feature)}>
+							<i class="far fa-headset"></i>
+
+							<div className={classNames(styles.info)}>
+								<div className={classNames(styles.title)}>Free Shipping</div>
+								<div className={classNames(styles.subtittle)}>From all orders over $100</div>
+							</div>
+						</div>
+
+						<div className={classNames(styles.feature)}>
+							<i class="far fa-badge-percent"></i>
+
+							<div className={classNames(styles.info)}>
+								<div className={classNames(styles.title)}>Free Shipping</div>
+								<div className={classNames(styles.subtittle)}>From all orders over $100</div>
+							</div>
+						</div>
+
+						<div className={classNames(styles.feature)}>
+							<i class="far fa-credit-card"></i>
+
+							<div className={classNames(styles.info)}>
+								<div className={classNames(styles.title)}>Free Shipping</div>
+								<div className={classNames(styles.subtittle)}>From all orders over $100</div>
+							</div>
+						</div>
+					</Carousel>
+				</div>
+			</div>
 		</div>
 	);
 }

53ef87ff7f1be93af6fce0c5e135e04bdd0425ed
Chopper356 <vedmedenko.nikita345@gmail.com>
2023-03-02 16:27:47 +0200

    added first home page banner and responsible

--- src/pages/App/index.jsx
+++ src/pages/App/index.jsx
@@ -1,10 +1,15 @@
 import Header from '../../components/Header';
+import Home from '../Home';
 import './app.module.scss';
 
 function App() {
   return (
     <div className="App">
       <Header />
+
+      <main>
+        <Home />
+      </main>
     </div>
   );
 }
